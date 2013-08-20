4b94212725da0aa6e35ba1502c652a595519b3fe
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2013-08-20 11:26:25 +0200

    Add formula for rsbag-tools-cl

---
+++ rsbag-tools-cl.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class RsbagToolsCl < Formula
+  homepage 'https://code.cor-lab.org/projects/rsbag'
+  url 'https://ci.cor-lab.org/view/rsb-0.9/job/rsbag-tools-cl-0.9-macos/label=MAC_OS_lion_64bit/lastSuccessfulBuild/artifact/build/bag', :using => :nounzip
+  version '0.9'
+  head 'https://ci.cor-lab.org/view/rsb-trunk/job/rsbag-tools-cl-trunk-macos/label=MAC_OS_lion_64bit/lastSuccessfulBuild/artifact/build/bag', :using => :nounzip
+  sha1 '07c09b594514f65419fd24795a2167a7b262caef'
+
+  def install
+    bin.install 'bag'
+    ln_s bin/'bag', bin/'bag-play'
+    ln_s bin/'bag', bin/'bag-cat'
+    ln_s bin/'bag', bin/'bag-info'
+    ln_s bin/'bag', bin/'bag-merge'
+    ln_s bin/'bag', bin/'bag-record'
+  end
+
+  def test
+    system "bag-play", "-h"
+  end
+end
