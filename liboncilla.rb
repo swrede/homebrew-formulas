d9daeac49d6227e53a278341110b4dc6a5a13eaa
Sebastian Wrede <sebwrede@googlemail.com>
2013-10-18 16:50:05 +0200

    workaround for compilation problem on MacOS

--- liboncilla.rb
+++ liboncilla.rb
@@ -1,6 +1,11 @@
 require 'formula'
 
 class Liboncilla < Formula
+  def patches
+    # fix for errorneous linker flag on MacOS 10.7
+    DATA
+  end
+
   homepage 'https://toolkit.cit-ec.uni-bielefeld.de'
   url 'https://redmine.amarsi-project.eu/git/quaddrivers.git', :using => :git
   version '0.3'
@@ -29,3 +34,22 @@ class Liboncilla < Formula
     system "false"
   end
 end
+
+__END__
+diff --git a/src/liboncilla/CMakeLists.txt b/src/liboncilla/CMakeLists.txt
+index 5338204..80c97b9 100644
+--- a/src/liboncilla/CMakeLists.txt
++++ b/src/liboncilla/CMakeLists.txt
+@@ -42,9 +42,8 @@ target_link_libraries(oncilla
+ set_target_properties(oncilla
+                       PROPERTIES
+                       VERSION ${VERSION_STRING}
+-                      SOVERSION ${VERSION_ABI}
+-                      LINK_FLAGS "-Wl,-unresolved-symbols=ignore-in-shared-libs")
+-
++                      SOVERSION ${VERSION_ABI})
++ 
+ install(TARGETS oncilla
+         EXPORT LibOncillaDepends
+         DESTINATION lib)
+
